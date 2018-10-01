a97439675577a9d70e7bd902dd664784485c224b
Erik van Renselaar <evanrenselaar@neit.edu>
2018-10-01 12:46:11 -0400

    Added real cool message

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "PHP is cool";
+        echo "PHP is really cool!!!";
        
         
         ?>
